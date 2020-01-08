f2cb63bcfdabf31026700154dc0967d9b31604f6
Evan Goldman <pitlere@yahoo.com>
2020-01-08 19:46:54 +0000

    added booksearch model

---
+++ app/models/booksearch.rb
@@ -0,0 +1,2 @@
+class Booksearch < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_133653) do
+ActiveRecord::Schema.define(version: 2020_01_08_194320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2019_12_18_133653) do
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "booksearches", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
